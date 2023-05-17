1ec90b949201d7f95c9dc14f8b75001a268366ab
Sandesh Mittal <sandesh.mittal@exxat.com>
2023-05-17 20:31:31 +0530

    End of Section 2

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1, "Sandesh Mittal");-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (2, "Swati Mittal");-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (3, "Pushpa Mittal");-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (4, "Satish Mittal");
---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] //  GET /api/Users
+    public class UsersController
+    {
+        private readonly DataContext context;
+
+        public UsersController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await this.context.Users.ToListAsync(); ;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await this.context.Users.FindAsync(id);
+        }
+    }
+}
