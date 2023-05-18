e0aa4e61500c42964d5e7e6514eb5bd82b4cc823
Sandesh Mittal <sandesh.mittal@exxat.com>
2023-05-18 19:20:54 +0530

    Formatting

--- API/Program.cs
+++ API/Program.cs
@@ -7,9 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt=>
+builder.Services.AddDbContext<DataContext>(opt =>
 {
-opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
 
